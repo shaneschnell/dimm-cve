449278f71c2af4ee15ef84a9843ea6359c01456f
Shane Schnell <2009431+shaneschnell@users.noreply.github.com>
2022-05-17 09:52:25 -0700

    add output

--- dimm_report.ps1
+++ dimm_report.ps1
@@ -18,10 +18,11 @@ Import-Module -Name Intersight.PowerShell
 # Setup Auth
 #================================================================================================
 # https://github.com/CiscoDevNet/intersight-powershell#authenticate-the-user
+# Please change the ApiKeyId and ApiKeyFilePath
 $onprem = @{
     BasePath = "https://intersight.com"
-    ApiKeyId = "xxxxx27564612d30dxxxxx/5f21c9d97564612d30dd575a/5f9a8b877564612xxxxxxxx"
-    ApiKeyFilePath = "C:\SecretKey.txt"
+    ApiKeyId = "xxxxx27564612d30dxxxxx/5f21c9d97564612d30dd575a/5f9a8b877564612xxxxxxxx" #Changeme
+    ApiKeyFilePath = "C:\SecretKey.txt" #Changeme
     HttpSigningHeader =  @("(request-target)", "Host", "Date", "Digest")
 }
 
@@ -32,3 +33,9 @@ Set-IntersightConfiguration @onprem
 # Collect Memory DIMM Info
 #================================================================================================
 Get-IntersightMemoryUnit | Where-Object {$_.Presence -ne "missing"} | Select-Object Model, Serial, Dn, Location | Sort-Object Dn | Export-Csv $CsvFile -NoTypeInformation
+
+
+#================================================================================================
+# Output
+#================================================================================================
+Write-Output "Report exported here: $csvFile"
